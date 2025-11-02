94e1b1f85e8c130a95673b9982aa067a71476156
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-11-02 13:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // write js code here if required
+const date=new Date();
+let calendar=`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+let time=date.toLocaleTimeString();
+
+const p=document.getElementById("timer");
+timer.innerText=`${calendar},${time}`
